4a71e1c2517ea109bb87b86d51140ddd840c2a54
Yong Ming <yongming1124@gmail.com>
2021-04-14 13:34:39 +0800

    Lab 6 done

--- Lab 5 - 20210407/Q3.js
+++ Lab 5 - 20210407/Q3.js
@@ -1,20 +1,31 @@
-class Movie{
-    constructor(title, rating, hasWatched){
-        this.title = title;
-        this.rating = rating;
-        this.hasWatched = hasWatched;
-    }
+// ES6 Object
+// class Movie{
+//     constructor(title, rating, hasWatched){
+//         this.title = title;
+//         this.rating = rating;
+//         this.hasWatched = hasWatched;
+//     }
 
-    print(){
-        console.log("You have "+(this.hasWatched?"":"not ")+"seen \""+this.title+"\" - "+this.rating+" stars");
-    }
+//     print(){
+//         console.log("You have "+(this.hasWatched?"":"not ")+"seen \""+this.title+"\" - "+this.rating+" stars");
+//     }
+// }
+
+// Traditional Object
+function Movie(title, rating, hasWatched) {
+    this.title = title;
+    this.rating = rating;
+    this.hasWatched = hasWatched;
+}
+Movie.prototype.print = function(){
+    console.log("You have "+(this.hasWatched?"":"not ")+"seen \""+this.title+"\" - "+this.rating+" stars");
 }
 
 var movieList = [];
-movieList.push(new Movie("In Bruges","5",true));
-movieList.push(new Movie("Frozen","4.5",false));
-movieList.push(new Movie("Mad Max Fury Road","5",true));
-movieList.push(new Movie("Les Miserables","3.5",false));
-movieList.forEach((movie)=>{
+movieList.push(new Movie("In Bruges", "5", true));
+movieList.push(new Movie("Frozen", "4.5", false));
+movieList.push(new Movie("Mad Max Fury Road", "5", true));
+movieList.push(new Movie("Les Miserables", "3.5", false));
+movieList.forEach((movie) => {
     movie.print();
 })
